7c2a05bca10c45e3550a4742f7f32a13e16d9c81
Chandan Singh <chandansingh@Chandans-MacBook-Pro.local>
2024-04-05 21:38:14 +0530

    Dev done

--- src/components/CurrenyConverter.jsx
+++ src/components/CurrenyConverter.jsx
@@ -8,12 +8,14 @@ const CurrenyConverter = () => {
 
   const [fromCurrency, setFromCurrency] = useState("USD");
   const [toCurrency, setToCurrency] = useState('USD');
+  const [convertedAmount, setConvertedAmount] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [favorites, setFavorites] = useState(['INR', 'USD'])
 
   const fetchCurrencies = async ()=>{
     try{
       const res = await fetch('https://api.frankfurter.app/currencies');
       const data = await res.json();
-      console.log('data',data);
       setCurrencies(Object.keys(data));
     } catch(err){
       console.log('error fetching currencies',err);
@@ -29,36 +31,59 @@ const CurrenyConverter = () => {
   // Latest -> https://api.frankfurter.app/latest?amount=1&from=USD&to=INR
   console.log('currencies', currencies);
 
-  const convertCurrency = ()=> {
+  const convertCurrency = async ()=> {
+    if(!amount) return;
+    setLoading(true);
+    try{
+      const res = await fetch(`https://api.frankfurter.app/latest?amount=${amount}&from=${fromCurrency}&to=${toCurrency}`);
+      const data = await res.json();
+      setConvertedAmount(data.rates[toCurrency]+ " " +toCurrency);
+    } catch(err){
+      console.log('error fetching currencies',err);
+    }finally{
+      setLoading(false)
+    }
 
   }
   const handleFavorite = (currency) =>{
+    let updatedFavorites = [...favorites];
+    if(favorites.includes(currency)){
+      updatedFavorites = updatedFavorites.filter((fav)=> fav!==currency)
+    }else{
+      updatedFavorites.push(currency);
+    }
+    setFavorites(updatedFavorites);
+    localStorage.setItem('favorites', JSON.stringify(updatedFavorites))
+  }
 
+  const swapCurrenies = ()=> {
+    setFromCurrency(toCurrency);
+    setToCurrency(fromCurrency);
   }
   return (
     <div className="max-w-xl my-auto p-5 bg-white rounded-lg shadow-md">
       <h2 className="mb-5 text-2xl font-semibold text-gray-700">Currency Converter</h2>
-      <div>
-        <CurrencyDropdown currencies={currencies} currency={fromCurrency} title="From:" handleFavourite={handleFavorite} setCurrency={setFromCurrency}/>
-        <div>
-          <button>
+      <div className="grid grid-cols-1 sm:grid-cols-3 items-end">
+        <CurrencyDropdown currencies={currencies} currency={fromCurrency} title="From:" favorites={favorites} handleFavourite={handleFavorite} setCurrency={setFromCurrency}/>
+        <div className="flex justify-center -mb-5 sm:mb-0">
+          <button className="p-2 bg-gray-200 rounded-full cursor-pointer hover:bg-gray-300" onClick={swapCurrenies}>
             <HiArrowsRightLeft/>
           </button>
         </div>
-        <CurrencyDropdown currencies={currencies} currency={toCurrency} title="To:" handleFavourite={handleFavorite} setCurrency={setToCurrency}/>
+        <CurrencyDropdown currencies={currencies} currency={toCurrency} title="To:" favorites={favorites} handleFavourite={handleFavorite} setCurrency={setToCurrency}/>
       </div>
       <div className="mt-4">
         <label htmlFor="amount" className="block text-sm font-medium text-gray-700">Amount</label>
         <input type='number' value={amount} onChange={e=> setAmount(e.target.value)} className="w-full p-2 border-gray-300 rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-indigo-400 mt-1"/>
       </div>
       <div className="flex justify-end mt-6">
-        <button className="px-5 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-7 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+        <button onClick={convertCurrency} className={`px-5 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-7 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 ${loading?'animate-pulse':''}`}>
           Convert
         </button>
       </div>
-      <div className="mt-4 text-lg font-medium text-right text-green-500">
-        Coverted Value: 59 USD
-      </div>
+      {convertedAmount && <div className="mt-4 text-lg font-medium text-right text-green-500">
+        Coverted Value: {convertedAmount}
+      </div>}
     </div>
   )
 }
--- src/components/DropDown.jsx
+++ src/components/DropDown.jsx
@@ -1,13 +1,21 @@
 /* eslint-disable react/prop-types */
-import {HiOutlineStar} from 'react-icons/hi2';
+import {HiOutlineStar, HiStar} from 'react-icons/hi2';
 
 const CurrencyDropdown = ({currencies, currency, setCurrency, favorites, handleFavourite, title = ""}) => {
-    console.log('currencies', currencies);
+
+const isFav = curr =>favorites.includes(curr);
   return (
     <div>
         <label htmlFor="title" className="block text-sm font-medium text-gray-700">{title}</label>
         <div className="mt-1 relative">
             <select value ={currency} onChange={e=>setCurrency(e.target.value)} className="w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus ring-2 focuse:ring-indigo-500">
+                {favorites && favorites.map(currency =>{
+                    return(
+                        <option key="currency" className='bg-gray-300'>
+                            {currency}
+                        </option> 
+                        )
+                })}
                 <hr />
                 {currencies && currencies.length && currencies.map(currency=>{
                 return(
@@ -20,7 +28,7 @@ const CurrencyDropdown = ({currencies, currency, setCurrency, favorites, handleF
             </select>
             <button onClick={()=> handleFavourite(currency)}
             className='absolute inset-y-0 right-0 pr-5 flex items-center text-sm leading-5'>
-                <HiOutlineStar/>
+                {isFav(currency)? <HiStar/>: <HiOutlineStar/>}
             </button>
         </div>
     </div>
